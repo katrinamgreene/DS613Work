24658c5755c7e0c9637feb2d233cd6c76cc7691e
Katrina Greene <96212774+katrinamgreene@users.noreply.github.com>
2022-07-25 19:29:32 -0400

    Update ForkingHomeworkRfile.R

--- ForkingHomeworkRfile.R
+++ ForkingHomeworkRfile.R
@@ -10,3 +10,10 @@ diamonds%>%
   filter(price > 7000 & price < 10000)%>%
   arrange(desc(carat))%>%
   slice(1:20)
+
+diamonds.dt <- data.table(diamonds)
+
+diamonds1 <- diamonds.dt[cut == "Premium" & price > 7000 & price < 10000, .(carat, cut, price)]
+diamonds2 <- diamonds1[order(-carat)]
+diamonds3 <- diamonds2[1:20]
+diamonds3
